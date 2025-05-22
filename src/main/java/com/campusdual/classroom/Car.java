cd52b41a00a2f77db5e51087738c788f461a35fe
NotOppi <Filippofg@outlook.com>
2025-05-22 14:26:10 +0200

    Listo

--- src/main/java/com/campusdual/classroom/Car.java
+++ src/main/java/com/campusdual/classroom/Car.java
@@ -1,5 +1,89 @@
 package com.campusdual.classroom;
 
 public class Car {
+    public String brand;
+    public String model;
+    public static final int MAX_SPEED = 120;
+    public String fuel;
+    public int speedometer = 0;
+    public int tachometer = 0;
+    public String gear = "N";
+    public boolean reverse = false;
+    public int wheelsAngle = 0;
+
+    public Car(String brand, String model, String fuel) {}
+
+    public Car() {}
+
+    public void start() {
+        if (this.tachometer == 0) {
+            this.tachometer = 1000;
+            System.out.println("Vehículo acendido");
+        } else {
+            System.out.println("O vehículo xa está acendido");
+        }
+    }
+
+    public void stop() {
+        if (this.speedometer == 0) {
+            this.tachometer = 0;
+            System.out.println("Vehículo apagado");
+        } else {
+            System.out.println("Non se pode apagar o vehículo, primeiro ten que estar detido");
+        }
+    }
+
+    public void accelerate() {
+        if ((this.speedometer + 40) > 120)
+            System.out.println("Maximun speed of the vehicle already reached. Cannot keep accelerating");
+        else
+            this.speedometer += 40;
+    }
+
+    public void brake() {
+        if ((this.speedometer - 40) < 0)
+            System.out.println("Cannot keep breaking. Vehicle already stopped");
+        else
+            this.speedometer -= 40;
+    }
+
+    public void turnAngleOfWheels(int angle) {
+        if ((this.wheelsAngle + angle) > 45)
+            this.wheelsAngle = 45;
+        else if ((this.wheelsAngle + angle) < -45)
+            this.wheelsAngle = -45;
+        else
+            this.wheelsAngle += angle;
+
+    }
+
+    public String showSteeringWheelDetail() {
+        return "";
+    }
+
+    public boolean isReverse() {
+        return this.gear.equals("R");
+    }
+
+    public void setReverse(boolean reverse) {
+        if (this.speedometer > 0)
+            System.out.println("Cannot set car to reverse gear. Car is still in motion.");
+        else
+            this.gear = "R";
+    }
+
+    public void showDetails() {}
+
+    public boolean isTachometerGreaterThanZero(){
+        return this.tachometer > 0;
+    }
+
+    public boolean isTachometerEqualToZero(){
+        return this.tachometer == 0;
+    }
+
+    public boolean isSpeedometerEqualToZero(){
+        return this.speedometer == 0;
+    }
 
 }
--- src/main/java/com/campusdual/classroom/Exercise12.java
+++ src/main/java/com/campusdual/classroom/Exercise12.java
@@ -3,6 +3,35 @@ package com.campusdual.classroom;
 public class Exercise12 {
     public static void main(String[] args) {
 
+        Car car1 = new Car("Hyundai", "Kona", "Gasolina");
+
+        if (car1.isTachometerGreaterThanZero())
+            System.out.println("Tachometer value: NOT Zero");
+        else
+            System.out.println("Tachometer value: Zero");
+        car1.start();
+        car1.start();
+        car1.stop();
+        car1.isTachometerGreaterThanZero();
+        if (car1.isTachometerEqualToZero())
+            System.out.println("Tachometer value: Zero");
+        else
+            System.out.println("Tachometer value: NOT Zero");
+        car1.start();
+        car1.accelerate();
+        car1.stop();
+        car1.accelerate();
+        car1.accelerate();
+        car1.accelerate();
+        car1.setReverse(car1.isSpeedometerEqualToZero());
+        car1.brake();
+        car1.brake();
+        car1.brake();
+        car1.brake();
+        car1.turnAngleOfWheels(20);
+        car1.turnAngleOfWheels(45);
+        car1.setReverse(car1.isSpeedometerEqualToZero());
+
     }
 
 }
